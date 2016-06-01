c0560b52fcb0e7376002da183071b4eef4f4e81e
wesley <xiaopihailaotou@gmail.com>
2016-06-01 16:41:18 +0800

    no commit too

--- ImageMark/src/com/wesley/imagemarker/Main.java
+++ ImageMark/src/com/wesley/imagemarker/Main.java
@@ -1,21 +1,6 @@
 package com.wesley.imagemarker;
 
-import java.awt.Color;
-import java.awt.FontMetrics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.RenderingHints;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
-
-import javax.imageio.ImageIO;
-
 import com.wesley.imagemarker.builder.BookCoverBuilder;
-import com.wesley.imagemarker.resource.ImageResource;
-import com.wesley.imagemarker.resource.TextResource;
 
 
 
@@ -24,8 +9,7 @@ public class Main {
 	public static void main(String[] args){
 
 	     
-	     
-		try {
+	   
 			String content = "若天压我，劈开那天，若地拘我，踏碎那地，我等生来自由身，谁敢高高在上.";
 			String cover = "f:\\img\\s9026255.jpg";
 			String targetFilePath = "f:\\img\\bookcover.jpg";
@@ -33,10 +17,7 @@ public class Main {
 			Templet templet = BookCoverBuilder.build(content,cover);
 			ImageMaker.build(templet,targetFilePath);
 			
-		}  catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 	     
 	     
 	}
